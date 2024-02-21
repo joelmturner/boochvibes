253bd758abfebcdc58ee702c48cfa08ef6b133e7
Joel Turner <joel@joelmturner.com>
2024-02-20 21:39:48 -0800

    update deps

--- src/routes/brew/[kombucha_id]/+page.server.ts
+++ src/routes/brew/[kombucha_id]/+page.server.ts
@@ -85,6 +85,6 @@ export const actions: Actions = {
 			});
 		}
 
-		throw redirect(303, `${url.pathname}?reviewedSuccessfully=true`);
+		redirect(303, `${url.pathname}?reviewedSuccessfully=true`);
 	},
 };
--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -4,8 +4,8 @@ export const POST: RequestHandler = async ({ locals }) => {
 	const { error: err } = await locals.supabase.auth.signOut();
 
 	if (err) {
-		throw error(500, 'Something went wrong logging you out.');
+		error(500, 'Something went wrong logging you out.');
 	}
 
-	throw redirect(303, '/');
+	redirect(303, '/');
 };
